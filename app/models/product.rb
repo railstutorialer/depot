acac6e0dba582509b6abb10abd85bc18ee925ca7
railstutorialer <railstutorialer15@gmail.com>
2016-10-30 11:12:55 +0100

    9

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -7,4 +9,14 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates_length_of :title, minimum: 10, too_short: 'Title must be at least 10 characters.'
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add :base, 'Line Items present'
+      throw :abort
+    end
+  end
 end
